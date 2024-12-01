d51215bd77ea34fda7ed4e1f52c31ae1b2cfefe7
Md. An Nahian Prince <130352614+CodderPrince@users.noreply.github.com>
2024-12-01 23:52:12 +0600

    Add files via upload

---
+++ a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    // Corrected scanf with &n
+    scanf("%d", &n);
+    printf("Hello Prince %d", n);
+
+    return 0;
+}
---
+++ a.py
@@ -0,0 +1,12 @@
+print("Hello Prince")
+n = input()
+print(n)
+""""""
+print("prince")
+'''
+prince'''
+print("hi")
+def hi():
+    """
+    _summary_
+    """    
